e450decc09e652cf53fef04013c59b2027b0b9ff
thomas <thomas.lang@lps-students.org>
2020-10-27 19:18:20 -0400

    Added keyboard support for those who don't have controllers Added the ability to drag and drop/ rotate the robot

--- robot/src/main/java/com/example/robot/auton.java
+++ robot/src/main/java/com/example/robot/auton.java
@@ -9,19 +9,7 @@ public class auton extends LinearOpMode {
     robot robert= RobotSimulator.robert;
     @Override
     public void runOpMode(){
-        while(opModeIsActive()){
-            if(RobotSimulator.keys.keys[KeyEvent.VK_W]){
-                for(int i=0; i<4; i++){
-                    robert.driveTrain[i].setPower(.8);
-                }
-            }
-            else{
-                for (int i=0; i<4; i++){
-                    robert.driveTrain[i].setPower(0);
-                }
-            }
-        }
-
+        
     }
 
 }
