53bec71142a5f77e2ea81c0d4e1078303310eba6
thomas <thomas.lang@lps-students.org>
2020-11-12 20:59:16 -0500

    Added support for multiple programs to be written

--- robot/src/main/java/programs/auton.java
+++ robot/src/main/java/programs/auton.java
@@ -5,6 +5,12 @@ import robot.LinearOpMode.*;
 import robot.RobotSimulator;
 import robot.robot;
 
+/** Use this as a model for all programs you want to write. Copy the imports down. The Autonomous
+ *  annotation at the beginning does nothing, but it makes it seem like a real program lol. Also
+ *  make sure any movement commands you wish to call get called on robert, as that is the only way
+ *  they will register
+ */
+
 @Autonomous(name = "auton", group ="f")
 public class auton extends LinearOpMode {
     robot robert= RobotSimulator.robert;
